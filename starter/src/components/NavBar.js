c5e258d2b2acd2b81cc117df26812ae5ed80365f
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-04-19 11:19:09 +0530

    added navbar

---
+++ starter/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Toolkit</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
